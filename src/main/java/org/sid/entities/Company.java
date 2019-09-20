fc65ea245ced524e68639575e2c9d1a369872043
Cedrick Kamsi <cedrick.kamsi@gmail.com>
2019-09-20 11:04:42 +0200

    First commit of the project

---
+++ src/main/java/org/sid/entities/Company.java
@@ -0,0 +1,45 @@
+package org.sid.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double price;
+	public Company() {
+		super();
+	}
+	public Company(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	public Long getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "Company [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
